0a1e42d5ccfd01238dd7efdbe4ce3012b100a32c
Anton Arhipov <ant.arhipov@gmail.com>
2019-01-09 19:57:07 +0100

    initial commit

---
+++ src/test/java/org/arhan/IntegrationTest.java
@@ -0,0 +1,23 @@
+package org.arhan;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class IntegrationTest extends TestCase {
+    public void testAp() {
+        assertTrue( true );
+    }
+
+    public void testApp() {
+        assertTrue( true );
+    }
+
+
+    public void testApppp() {
+        assertTrue( true );
+    }
+}
